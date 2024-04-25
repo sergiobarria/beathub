e748da4aff5f192396cb248cd20c398d5298b8e3
sergiobarria <sbarria.dev@gmail.com>
2024-04-25 16:27:34 -0500

    sveltekit app setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DJ Block
+
+> Full stack application for a showcase of DJ's events and music.
+
+## Table of Contents
+
+## Installation
+
+## Setup
+
+```bash
+$ bun create svelte@latest
+$ bun install
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<Button on:click={() => alert('IT WORKS!')}>Click me!</Button>
