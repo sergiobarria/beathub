26e9bf19f0a2a89c4f1038cb342595c95f47a57f
sergiobarria <sbarria.dev@gmail.com>
2024-04-29 12:28:16 -0500

    generate new migrations

--- drizzle/migrations/0000_slippery_the_professor.sql
+++ drizzle/migrations/0000_slippery_the_professor.sql
@@ -1,26 +1,15 @@
-CREATE TABLE `event_images` (
-	`id` text PRIMARY KEY NOT NULL,
-	`event_id` text NOT NULL,
-	`object_key` text NOT NULL,
-	`caption` text,
-	`filename` text NOT NULL,
-	`mimetype` text NOT NULL,
-	`size` integer NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	FOREIGN KEY (`event_id`) REFERENCES `events`(`id`) ON UPDATE no action ON DELETE cascade
-);
---> statement-breakpoint
 CREATE TABLE `events` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
 	`slug` text NOT NULL,
 	`venue` text NOT NULL,
+	`performers` text NOT NULL,
 	`date` text NOT NULL,
 	`description` text NOT NULL,
 	`time` text NOT NULL,
 	`street` text NOT NULL,
 	`city` text NOT NULL,
+	`cover` text,
 	`state` text NOT NULL,
 	`zip` text NOT NULL,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
